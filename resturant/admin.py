ba6473cfe335f4da573dba3e5d8904f2f39bcc37
Ismino <denise_cigel@hotmail.com>
2023-08-28 20:07:26 +0000

    Add adminuser and admin panel

--- resturant/admin.py
+++ resturant/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Table, Reservation
 
-# Register your models here.
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('seats', 'min_people', 'max_people',)
+    list_filter = ('seats', 'min_people', 'max_people',)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('table', 'customer', 'reservation_datetime', 'status',)
+    list_filter = ('table', 'customer', 'reservation_datetime','status',)
+    search_fields = ('customer__username',)
