7c36d9acfa9b58d4a3081d6b0a089b0747de79db
Ismino <denise_cigel@hotmail.com>
2023-08-28 20:23:29 +0000

    Add to adminpanel

--- resturant/admin.py
+++ resturant/admin.py
@@ -5,9 +5,21 @@ from .models import Table, Reservation
 class TableAdmin(admin.ModelAdmin):
     list_display = ('seats', 'min_people', 'max_people',)
     list_filter = ('seats', 'min_people', 'max_people',)
+    search_fields = ('customer__username', 'customer')
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     list_display = ('table', 'customer', 'reservation_datetime', 'status',)
     list_filter = ('table', 'customer', 'reservation_datetime','status',)
-    search_fields = ('customer__username',)
+    search_fields = ('customer__username', 'customer')
+    actions = ['cancel_reservations', 'mark_completed']
+
+    def cancel_reservations(self, request, queryset):
+        queryset.update(status=2)  
+    cancel_reservations.short_description = "Cancel selected reservations"
+
+    def mark_completed(self, request, queryset):
+        queryset.update(status=3)  
+    mark_completed.short_description = "Mark selected reservations as Completed"
+
+
