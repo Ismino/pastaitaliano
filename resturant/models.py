99e814d70169c2753d63a06ba891b2ffbcbfd28f
Ismino <denise_cigel@hotmail.com>
2023-08-28 09:48:22 +0000

    Add comments to models

--- resturant/models.py
+++ resturant/models.py
@@ -5,19 +5,21 @@ from django.core.validators import MinValueValidator
 from django.utils import timezone
 
 # Define choices for reservation status
+# Inspired by CodeInstitute models for Django Blog app
 STATUS_CHOICES = (
     (0, 'Pending'),
     (1, 'Confirmed'),
     (2, 'Cancelled'),
 )
 
+# Class for the resturant name to be used in relations to the other Classes
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
-    # Add other restaurant information fields here
 
     def __str__(self):
         return self.name
 
+# Class for the Table that handels the seats and info of the table 
 class Table(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     seats = models.IntegerField()
@@ -27,13 +29,15 @@ class Table(models.Model):
     def __str__(self):
         return f"Table at {self.restaurant.name} - Seats: {self.seats}"
 
+#Class fot the reservaton info and a validation error if the reservation is done in the past
 class Reservation(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     reservation_datetime = models.DateTimeField()
     status = models.IntegerField(choices=STATUS_CHOICES, default=0)
 
-    def clean(self):
+    # Inspiered method from https://github.com/renatalantos/booking-system/blob/main/restaurant/models.py  
+    def ValidationHandler(self):
         if self.reservation_datetime < timezone.now():
             raise ValidationError("Reservation date and time cannot be in the past.")
 
