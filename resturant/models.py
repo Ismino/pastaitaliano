8f1160e3bd5b6b7233920d4a05e0d43db58db47c
Ismino <denise_cigel@hotmail.com>
2023-08-29 07:50:59 +0000

    Add imports to views

--- resturant/models.py
+++ resturant/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.core.validators import MinValueValidator
 from django.utils import timezone
 
 # Define choices for reservation status
--- resturant/views.py
+++ resturant/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Table, Reservation
 
 # Create your views here.
