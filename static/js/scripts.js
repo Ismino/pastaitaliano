e066fcf3c752f0b602c3e54498c7d9498bb02618
Ismino <denise_cigel@hotmail.com>
2023-10-04 18:50:06 +0000

    Add map

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -13,3 +13,25 @@ $(document).ready(function () {
     });
 });
 
+/* Map */
+
+function initMap() {
+    var stockholm = { lat: 59.3293, lng: 18.0686 }; // Stockholm coordinates
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: stockholm
+    });
+
+    var marker = new google.maps.Marker({
+        position: stockholm,
+        map: map,
+        title: 'Stockholm'
+    });
+
+}
+window.onload = function() {
+    initMap();
+}
+
+
+
